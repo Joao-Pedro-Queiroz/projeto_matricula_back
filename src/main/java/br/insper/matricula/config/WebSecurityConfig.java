11bd4f0344f6ec57cfe66f6d83a4e3659188a493
Joao-Pedro-Queiroz <jpqv0105@gmail.com>
2025-05-06 15:56:56 -0300

    Concertando back

--- src/main/java/br/insper/matricula/config/WebSecurityConfig.java
+++ src/main/java/br/insper/matricula/config/WebSecurityConfig.java
@@ -17,7 +17,11 @@ public class WebSecurityConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*");
+        registry.addMapping("/**")
+                .allowedOrigins("*") // ou "http://localhost:5173" se quiser limitar
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 
     @Bean
